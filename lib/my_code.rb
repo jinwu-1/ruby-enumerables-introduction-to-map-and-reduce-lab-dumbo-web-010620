9f990e5904c5d60ce5d53785339f1188ccfb4dd9
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 18:13:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,13 @@ def map_to_negativize(source_array)
   end
   new_array
 end
+
+def map_to_no_change(source_array)
+  new_array = []
+  counter = 0
+  while counter < source_array.length
+    new_array.push(source_array[counter])
+    counter += 1
+  end
+  new_array
+end
