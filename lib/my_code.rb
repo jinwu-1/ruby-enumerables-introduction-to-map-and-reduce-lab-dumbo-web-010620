87f40eec038659389f44355d42aece81af479711
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 18:18:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,3 +37,13 @@ def map_to_square(source_array)
   end
   new_array
 end
+
+def reduce_to_total(source_array, starting_point)
+  sum = starting_point
+  counter = 0
+  while counter < source_array.length
+    sum += source_array[counter]
+    counter += 1
+  end
+  sum
+end
