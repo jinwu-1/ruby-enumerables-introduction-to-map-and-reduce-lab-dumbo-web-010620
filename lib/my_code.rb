461e4f63a9fcf5b88e54d1ef8529d70b775caba6
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 18:14:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,3 +27,13 @@ def map_to_double(source_array)
   end
   new_array
 end
+
+def map_to_square(source_array)
+  new_array = []
+  counter = 0
+  while counter < source_array.length
+    new_array.push(source_array[counter] ** 2)
+    counter += 1
+  end
+  new_array
+end
