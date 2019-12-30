bac1054cbf9f23889b9aa203502c75032430d724
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 18:26:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,7 +57,7 @@ def reduce_to_all_true(source_array)
   return true
 end
 
-def reduce_to_all_true(source_array)
+def reduce_to_any_true(source_array)
   counter = 0
   while counter < source_array.length
     return true if source_array[counter] === true
